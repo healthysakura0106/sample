0f79de61301819891dd33c2f81adb0922c1cf0fd
unknown <magnet0106@docomo.ne.jp>
2018-06-23 19:29:34 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+  def index
+    @posts=Post.all.order(created_at: :desc)
+
+  end
+  def show
+    @post = Post.find_by(id:params[:id])
+  end
+  def new
+  end
+  def create
+    @post=Post.new(title:params[:title])
+
+    @post.save
+    redirect_to("/posts/index")
+  end
+end
